d1cf7ea330bca06e356911454043a858319c9903
Bhumit Desai <bhumitd9@gmail.com>
2024-12-26 23:09:27 +0000

    phase 1 of building app - dashboard added and navbar functions

---
+++ pokevault/client/app/(components)/Navbar/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full md-7">
+        {/*/ LEFT SIDE */}
+        <div className="flex justify-between items-center gap-5">
+            <button 
+            className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
+            onClick={() => {}}
+            >
+
+            
+            </button>
+        </div>
+       </div>
+  );
+};
+
+export default Navbar;
