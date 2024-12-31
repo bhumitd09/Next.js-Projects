c4067f4a213e4f0eb2d1b2f437ca697c128a1069
Bhumit Desai <bhumitd9@gmail.com>
2024-12-31 18:01:41 +0000

    Prisma added

--- pokevault/client/app/(components)/Sidebar/index.tsx
+++ pokevault/client/app/(components)/Sidebar/index.tsx
@@ -8,7 +8,7 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 
 interface SidebarLinkProps {
-    href: string; // Fixed typo
+    href: string;
     icon: LucideIcon;
     label: string;
     isCollapsed: boolean;
@@ -25,17 +25,18 @@ const SidebarLink = ({
 
     return (
         <Link href={href}>
-            <div className={`cursor-pointer flex items-center ${
-                isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
-                } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
-                    isActive ? "bg-blue-200 text-white" : ""
+            <div
+                className={`cursor-pointer flex items-center ${
+                    isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+                } hover:text-blue-500 hover:bg-blue-100 dark:hover:bg-blue-800 gap-3 transition-colors ${
+                    isActive ? "bg-blue-200 text-white dark:bg-blue-900" : "text-gray-900 dark:text-gray-300"
                 }`}
             >
-                {/* Set the icon color to blue */}
-                <Icon className="w-6 h-6 text-blue-500" />
-                <span className={`${
-                    isCollapsed ? "hidden" : "block"
-                } font-medium text-gray-700`}
+                <Icon className="w-6 h-6 text-blue-500 dark:text-blue-400" />
+                <span
+                    className={`${
+                        isCollapsed ? "hidden" : "block"
+                    } font-medium text-gray-900 dark:text-gray-300`}
                 >
                     {label}
                 </span>
@@ -56,28 +57,30 @@ const Sidebar = () => {
 
     const sidebarClassNames = `fixed flex flex-col ${
         isSideBarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
-    } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+    } bg-gray-100 dark:bg-blue-900 transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
 
     return (
         <div className={sidebarClassNames}>
             {/* TOP LOGO */}
-            <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
-                isSideBarCollapsed ? "px-5" : "px-6"
-            }`}
+            <div
+                className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+                    isSideBarCollapsed ? "px-5" : "px-6"
+                }`}
             >
-                <div>logo</div>
-                <h1 className={`${
-                    isSideBarCollapsed ? "hidden" : "block"
-                } font-extrabold text-2xl`}
+                <div className="text-gray-900 dark:text-gray-100">logo</div>
+                <h1
+                    className={`${
+                        isSideBarCollapsed ? "hidden" : "block"
+                    } font-extrabold text-2xl text-gray-900 dark:text-gray-100`}
                 >
                     PokéVault
                 </h1>
                 <button
-                    className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-pink-200"
+                    className="md:hidden px-3 py-3 bg-gray-200 rounded-full hover:bg-pink-200 dark:bg-gray-700 dark:hover:bg-pink-500"
                     onClick={toggleSidebar}
                     aria-label="Toggle Sidebar"
                 >
-                    <Menu className='w-4 h-4' />
+                    <Menu className="w-4 h-4 text-gray-900 dark:text-gray-100" />
                 </button>
             </div>
 
@@ -92,8 +95,8 @@ const Sidebar = () => {
             </div>
 
             {/* Footer */}
-            <div className={`${isSideBarCollapsed  ? "hidden" : "block"} md-10`}>
-                <p className='text-center text-xs text-blue-500'> {/* change className on p to change color or footer text. */}
+            <div className={`${isSideBarCollapsed ? "hidden" : "block"} md-10`}>
+                <p className="text-center text-xs text-pink-400 dark:text-white-500">
                     &copy; 2024 PokéVault | Site By OneDev
                 </p>
             </div>
--- pokevault/client/next.config.ts
+++ pokevault/client/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+};
+
+module.exports = {
+  images: {
+    domains: ['media.licdn.com'], // Add the LinkedIn domain here
+  },
 };
 
 export default nextConfig;
